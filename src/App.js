84297f6ea708add0800157e5e6acf9a91aefa21e
sherlvick <kvicky047@gmail.com>
2023-11-09 20:00:30 +0530

    [Login] - Google login test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form
+      className="text-center"
+      method="post"
+      action="/login/federated/google"
+    >
+      <button type="submit" class="btn btn-primary">
+        Sign In With Google
+      </button>
+    </form>
   );
 }
 
